7d5a8d8ec3292b2fd21823d705c5d93423e97874
Rahul <rkc.98@outlook.com>
2021-10-26 08:01:15 +0530

    minor content changed

--- src/emails/accounts.js
+++ src/emails/accounts.js
@@ -1,20 +1,12 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-// sgMail.setApiKey(apiKey);
-// const msg = {
-//   to: "choudharyrahul041@gmail.com", // Change to your recipient
-//   from: "rkc.98@outlook.com", // Change to your verified sender
-//   subject: "Sending with SendGrid is Fun",
-//   text: "and easy to do anywhere, even with Node.js",
-//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
-// };
 
 const sendEmail = (email, user) => {
   const msg = {
     to: email,
     from: "rkc.98@outlook.com",
     subject: "Thank you for registering with us",
-    text: `Welcome to the app, ${user} .Let me know your review on the app `,
+    text: `Welcome to the app, ${user} .Let me know your review on Task Manager App `,
   };
   sgMail
     .send(msg)
